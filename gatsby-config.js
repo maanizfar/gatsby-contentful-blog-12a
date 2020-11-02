8875543131f3c5a891f2cba648adc960b98d4720
Salman Zafar <maanizfar@gmail.com>
2020-11-02 14:29:37 +0500

    connected contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Contentful Blog`,
@@ -27,9 +29,18 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    }, // this (optional) plugin enables Progressive Web App + Offline functionality
+    },
+    "gatsby-plugin-typescript",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-transformer-remark`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    "gatsby-plugin-typescript",
   ],
 }
